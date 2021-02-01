26a8d825dc4d912e310a61986e4018e9dea3f81a
Amal sekri <amal.sekri@gmail.com>
2021-02-01 21:41:05 +0100

    fff
    fff

---
+++ Exercices/01.js
@@ -0,0 +1,9 @@
+function calcul(a,b){
+    let total = a + b ;
+    return total;
+}
+function affichage(total,a,b){
+    document.write("Le résultat de  " + a + " + " + b + " est : " + total);
+}
+let recup = calcul(10,30);
+
